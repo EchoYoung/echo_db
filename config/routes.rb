090982e3c21f3d4439d5cef3777ed63df4660c9b
Chang Young Park <cypark@cs.stanford.edu>
2014-07-30 12:01:30 -0700

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 EchoDb::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root 'echo#home'
   match '/signup', to: 'users#new',     via: 'get'
+  match '/signin', to: 'sessions#new',  via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
   
   
   
